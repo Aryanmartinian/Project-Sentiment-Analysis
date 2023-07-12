d266ad61b68f4e246635f6388d155c86fe335d3c
Aryan <aryan.martinian@gmail.com>
2023-07-12 09:33:15 +0530

    first commit

---
+++ pages/About.py
@@ -0,0 +1,3 @@
+#st.markdown('This application is all about tweet sentiment analysis of airlines.We can analyse reviews of the passengers using this streamlit application')
+import streamlit as st
+st.markdown('This application is all about Tweet Sentiment Analysis of Airlines. ')
